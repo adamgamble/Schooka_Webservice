7c69baf880f99afd646445e1e78e35b7c33cbb7a
Adam Gamble <adamgamble@gmail.com>
2011-07-02 10:35:30 -0500

    initial repo

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '~> 3.1.0.rc4'
+
+gem 'pg', "~> 0.11.0"
+
+# Asset template engines
+gem 'sass-rails', "~> 3.1.0.rc"
+gem 'coffee-script', "~> 2.2.0"
+gem 'uglifier', "~> 1.0.0"
+gem 'jquery-rails', "~> 1.0.12"
+
+group :test, :development do
+  gem 'turn', "~> 0.8.2", :require => false
+  gem 'ruby-debug19', "~> 0.11.6"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SchookaWebservice::Application.config.secret_token = '9767534224f51be2f6ab48961ad3df60fc035a5033633cadef6ef5eb471a9b5c2bcb6724d5a70488de60dcb36d4bba104eb42bc2e13d10a1a2848850bebc1db0'
